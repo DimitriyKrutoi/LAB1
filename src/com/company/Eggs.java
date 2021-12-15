2aab4c2d0169ff5c7323e470c0db938c6f30e90c
Dima <you@example.com>
2021-12-16 02:29:56 +0300

    new eggs

--- src/com/company/Eggs.java
+++ src/com/company/Eggs.java
@@ -1,4 +1,21 @@
 package com.company;
 
-public class Eggs {
+public class Eggs  implements Nutritious{
+    int calories;
+    String number;
+
+    public Eggs(String eggsNumber, int eggsCalories)
+    {
+        this.number = eggsNumber;
+        this.calories=eggsCalories;
+    }
+    public void system_print(){
+
+        System.out.println("Число: "+ number+",калории: "+calories);
+    }
+
+    @Override
+    public int calculateCalories(){
+        return this.calories;
+    }
 }
